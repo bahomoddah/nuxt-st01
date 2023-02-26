72968153341d22865b6ff401bdfd1868902112fd
Bahomoddah <bahomoddah@gmail.com>
2023-02-26 11:58:13 +0300

    make todo functions as composable

--- todo-app/composables/states.js
+++ todo-app/composables/states.js
@@ -1,14 +1,73 @@
+export const useToDo = () => {
+  const url = "http://localhost:4000";
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: "Basic enVja2VyOjEyMzQ1Ng==",
+    auth: {
+      username: "felon",
+      password: "123123",
+    },
+  };
+  const todos = ref([]);
+  const newTask = ref("");
+  fetchList();
 
-    
-
-  console.log("prr", process.server);
-  // let user = {}
-  if (!process.server) {
-    const user = JSON.parse(localStorage.getItem("USER") || "{}") 
-    console.log("user auth ", user);
+  function addNewTask() {
+    if (newTask.value) {
+      $fetch(`/todos`, {
+        method: "POST",
+        baseURL: url,
+        params: {},
+        body: {
+          task: newTask.value,
+        },
+        headers,
+      }).then(() => {
+        fetchList();
+      });
+      newTask.value = "";
+    }
   }
-export const useAuth = () => 
+  function markAsCompleted(id) {
+    $fetch(`/todos/${id}`, {
+      method: "PUT",
+      baseURL: url,
+      headers,
+    }).then(() => {
+      fetchList();
+    });
+  }
+  function removeTodo(id) {
+    $fetch(`/todos/${id}`, {
+      method: "DELETE",
+      baseURL: url,
+      headers,
+    }).then(() => {
+      fetchList();
+    });
+  }
+  function fetchList() {
+    $fetch(`/todos`, {
+      method: "GET",
+      baseURL: url,
+      headers,
+    }).then((data) => {
+      todos.value = data;
+    });
+  }
+
+  return {
+    todos,
+    newTask,
+    fetchList,
+    addNewTask,
+    markAsCompleted,
+    removeTodo,
+  };
+};
+export const useAuth = () =>
   useState(() => ({
-    isAuthenticated: false
+    isAuthenticated: false,
+    profile: JSON.parse(localStorage.getItem("USER") || "{}")
   }));
-  
--- todo-app/pages/index.vue
+++ todo-app/pages/index.vue
@@ -25,74 +25,18 @@
 </template>
 
 <script setup>
-const user = ref({});
+// check user is login
 definePageMeta({
   middleware: ["auth"],
 });
 
-if (!process.server) {
-  user.value = JSON.parse(localStorage.getItem("USER") || "{}");
-  if (!user.value.id) {
-    navigateTo("/login");
-  }
-}
-const url = "http://localhost:4000";
-const headers = {
-  "Content-Type": "application/json",
-  Accept: "application/json",
-  Authorization: "Basic enVja2VyOjEyMzQ1Ng==",
-  auth: {
-    username: "felon",
-    password: "123123",
-  },
-};
-const todos = ref([]);
-const newTask = ref("");
-fetchList();
+const auth = useAuth();
+const user = computed(() => auth.value.profile);
+!user.value.id ? navigateTo("/login") : "";
 
-function addNewTask() {
-  if (newTask.value) {
-    $fetch(`/todos`, {
-      method: "POST",
-      baseURL: url,
-      params: {},
-      body: {
-        task: newTask.value,
-      },
-      headers,
-    }).then(() => {
-      fetchList();
-    });
-    newTask.value = "";
-  }
-}
-function markAsCompleted(id) {
-  $fetch(`/todos/${id}`, {
-    method: "PUT",
-    baseURL: url,
-    headers,
-  }).then(() => {
-    fetchList();
-  });
-}
-function removeTodo(id) {
-  $fetch(`/todos/${id}`, {
-    method: "DELETE",
-    baseURL: url,
-    headers,
-  }).then(() => {
-    fetchList();
-  });
-}
-function fetchList() {
-  $fetch(`/todos`, {
-    method: "GET",
-    baseURL: url,
-    headers,
-  }).then((data) => {
-    todos.value = data;
-  });
-}
+const { todos, newTask, fetchList, addNewTask, markAsCompleted, removeTodo } =
+  useToDo();
+
 useHead({
   title: "ToDo List App",
 });
